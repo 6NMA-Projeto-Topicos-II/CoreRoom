801f17007e49548a104ba956071f8d475b1512e4
renzotakada <renzotakada1@gmail.com>
2023-09-17 12:31:10 -0300

    feat: criando adapter mongo db

---
+++ CoreRoom/Adapters/MongoDBAdapter/Connections/MongoSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreRoom.Adapters.MongoDBAdapter.Connections
+{
+    public class MongoSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string CollectionName { get; set; }
+    }
+}
---
+++ CoreRoom/Infrastructure/Entensions/MongoDBExtensions.cs
@@ -0,0 +1,14 @@
+﻿using CoreRoom.Adapters.MongoDBAdapter.Connections;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoreRoom.Infrastructure.Entensions
+{
+    public static class MongoDBExtensions
+    {
+        public static void AddMongoDB(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddSingleton(services.Configure<MongoSettings>(configuration.GetSection("MongoDBDatabase")));
+        }
+    }
+}
