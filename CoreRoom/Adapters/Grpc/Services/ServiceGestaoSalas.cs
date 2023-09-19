2db48f3dc35bc95af4c3828a83e93c4f8d00d82b
DitoCarrera <vitorcarreraya@gmail.com>
2023-09-19 00:27:45 -0300

    Criando a classe de Serviço de Gestao de Salas (ServiceGestaoSalas)

---
+++ CoreRoom/Adapters/Grpc/Services/ServiceGestaoSalas.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+
+namespace CoreRoom.Adapters.Grpc.Services
+{
+    public class ServiceGestaoSalas : GestaoSalasService.GestaoSalasServiceBase
+    {
+        public override Task<BaseStatus> AlocarSala(BodyRequestSala request, ServerCallContext context)
+        {
+            return base.AlocarSala(request, context);
+        }
+
+        public override Task<BaseStatus> ConsultarSala(BodyRequestSala request, ServerCallContext context)
+        {
+            return base.ConsultarSala(request, context);
+        }
+        public override Task<BaseStatus> ConsultarSalaAlocada(BodyRequestSala request, ServerCallContext context)
+        {
+            return base.ConsultarSalaAlocada(request, context);
+        }
+
+        public override Task<BaseStatus> Desalocar(BodyRequestSala request, ServerCallContext context)
+        {
+            return base.Desalocar(request, context);
+        }
+
+        public override Task<BaseStatus> ListarSalaAlocada(BodyRequestSala request, ServerCallContext context)
+        {
+            return base.ListarSalaAlocada(request, context);
+        }
+
+    }
+}
