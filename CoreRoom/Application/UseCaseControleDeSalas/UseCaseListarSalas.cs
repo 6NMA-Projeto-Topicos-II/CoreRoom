22abcd9adc5581a76f3a14eacd7d706e75a52576
renzotakada <renzotakada1@gmail.com>
2023-09-25 21:22:49 -0300

    feat: useCase listar Salas para controle

--- CoreRoom/Application/UseCaseControleDeSalas/UseCaseListarSalas.cs
+++ CoreRoom/Application/UseCaseControleDeSalas/UseCaseListarSalas.cs
@@ -1,6 +1,8 @@
-﻿using CoreRoom.Domain.Dto.ControleSalasDto;
+﻿using CoreRoom.Application.Mapper;
+using CoreRoom.Domain.Dto.ControleSalasDto;
 using CoreRoom.Ports.InputboundPort;
 using CoreRoom.Ports.OutboundPort;
+using System.Text.Json;
 
 namespace CoreRoom.Application.UseCaseControleDeSalas
 {
@@ -10,9 +12,31 @@ namespace CoreRoom.Application.UseCaseControleDeSalas
         {
         }
 
-        public Task<string> ListRoom(inputControleSalas input)
+        public async Task<string> ListRoom(inputControleSalas input)
         {
-            throw new NotImplementedException();
+            var mapper = MapperControleSalas.ForRepository(input);
+
+            var ret = await _repository.FindBlock(mapper);
+
+            var listaRetorno = new List<ResponseConsultaSalasControle>();
+
+            ret.InfAndares.ToList().ForEach(x =>
+            {
+                var RetMapSala = new ResponseConsultaSalasControle
+                {
+                    bloco = ret.Bloco,
+                    Bloqueada = x.Bloqueada,
+                    capacidadeDeAlunos = x.CapacidadeDeAlunos,
+                    EmAula = x.Ativa,
+                    NumeroDaSala = x.NumeroSala,
+                    numeroDoAndar = x.Andar,
+                };
+                listaRetorno.Add(RetMapSala);
+            });
+
+            var listaSerializada = JsonSerializer.Serialize(listaRetorno);
+
+            return listaSerializada;
         }
     }
 }
